6c95ae6a27695f23c592f8f6e179baa3a565a560
Caleeeb <daycinematography@gmail.com>
2022-07-09 17:20:59 -0400

    deploying heroku

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI \\ 'mongodb://localhost:27017/budget-tracker', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
   useFindAndModify: false
 });
 
